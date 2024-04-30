651640d06d3f39e8155e4d9786906e6d8bd524d5
Angello Hernandez <amhs0204@gmail.com>
2024-04-30 03:37:54 -0300

    first commit

---
+++ project/config/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse 
+from django.template import Context , Template
+from django.shortcuts import render
+
+def saludo(request):
+    return HttpResponse ("Hola django")
+
+
+def probando_template(request):
+    mi_html = open("./templates/template1.html")
+    mi_template = Template(mi_html.read()) 
+    mi_html.close()
+    mi_contexto = Context()
+    mi_documento = mi_template.render(mi_contexto)
+    return HttpResponse(mi_documento)
+
+
+def mis_notas(request):
+    
+    return render(request ,"notas.html")
